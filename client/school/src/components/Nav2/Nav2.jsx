e85abc870b344f0f61a972b33019aeb4983c6568
Roman <145358556+Rade16@users.noreply.github.com>
2023-10-13 01:05:57 +0300

    Added hamburger menu (beta)
    Added hamburger menu (beta)

--- client/school/src/components/Nav2/Nav2.jsx
+++ client/school/src/components/Nav2/Nav2.jsx
@@ -1,16 +1,41 @@
-import './_nav2.scss'
-import logo from './../../img/logo.svg';
+import "./_nav2.scss";
+import logo from "./../../img/logo.svg";
 
 const Nav2 = () => {
   return (
-    <header className='header__nav'>
-      <div className='header__container__nav'>
+    <header className="header__nav">
+      <div className="header__container__nav">
+        <nav className="nav">
+          <div className="nav__menu" >
+            <input type="checkbox" className="nav__checkbox"/>
+            <p className="nav-link nav-link_menu">Меню</p>
 
-        <nav className='nav'>
-            <div className="nav__menu">
-                <p className='nav-link'>Меню</p>
-            </div>
-        <ul className="nav__list">
+            <ul className="menu" >
+              <li>
+                <a href="#">Профиль</a>
+              </li>
+              <li>
+                <a href="#">Мои курсы</a>
+              </li>
+              <li>
+                <a href="#">Пройденный материал</a>
+              </li>
+              <li>
+                <a href="#">Статистика</a>
+              </li>
+              <li>
+                <a href="#">Моя группа</a>
+              </li>
+              <li>
+                <a href="#">Настройки</a>
+              </li>
+              <li>
+                <a href="#">Выход</a>
+              </li>
+            </ul>
+          </div>
+
+          <ul className="nav__list">
             <li>
               <a className="nav-link" href="#">
                 Главная
