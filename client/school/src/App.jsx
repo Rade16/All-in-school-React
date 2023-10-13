1a0e02fb75a198826d5d71df979a1e9c4c8f5042
Denver <74683888+Danya200221@users.noreply.github.com>
2023-10-13 13:50:34 +0300

    Added new page: courses

--- client/school/src/App.jsx
+++ client/school/src/App.jsx
@@ -1,24 +1,24 @@
-
 import React from 'react';
 import './scss/main.scss';
-
 
 import Header from './pages/Header/Header';
 import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
 // import Nav from "./components/Nav/Nav";
-import Reg from "./pages/Reg/Reg.jsx";
+import Reg from './pages/Reg/Reg.jsx';
 import Nav2 from './components/Nav2/Nav2';
 import Profile from './pages/Profile/Profile';
+import Courses from './pages/Courses/Courses';
 function App() {
   return (
     <>
-      <Nav2/>
-        {/* <Nav /> */}
-        {/*<Reg/>*/}
+      <Nav2 />
+      {/* <Nav /> */}
+      {/*<Reg/>*/}
       {/* <Header />
       <Home /> */}
-      <Profile/>
+      {/* <Profile/> */}
+      <Courses />
       <Footer />
     </>
   );
