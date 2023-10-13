bb0dbd245fb15ef961b6e0eb1732de9992549caa
Denver <74683888+Danya200221@users.noreply.github.com>
2023-10-13 11:50:43 +0300

    Fixed

--- client/school/src/components/Nav2/Nav2.jsx
+++ client/school/src/components/Nav2/Nav2.jsx
@@ -1,58 +1,57 @@
-import "./_nav2.scss";
-import logo from "./../../img/logo.svg";
+import './_nav2.scss';
+import logo from './../../img/logo.svg';
 
 const Nav2 = () => {
   return (
-    <header className="header__nav">
-      <div className="header__container__nav">
-        <nav className="nav">
-          <div className="nav__menu" >
-            <input type="checkbox" className="nav__checkbox"/>
-            <p className="nav-link nav-link_menu">Меню</p>
+    <header className='header__nav'>
+      <div className='header__container__nav'>
+        <nav className='nav'>
+          <div className='nav__menu'>
+            <input type='checkbox' className='nav__checkbox' />
+            <p className='nav-link nav-link_menu'>Меню</p>
 
-            <ul className="menu" >
+            <ul className='menu'>
               <li>
-                <a href="#">Профиль</a>
+                <a href='#'>Профиль</a>
               </li>
               <li>
-                <a href="#">Мои курсы</a>
+                <a href='#'>Мои курсы</a>
               </li>
               <li>
-                <a href="#">Пройденный материал</a>
+                <a href='#'>Пройденный материал</a>
               </li>
               <li>
-                <a href="#">Статистика</a>
+                <a href='#'>Статистика</a>
               </li>
               <li>
-                <a href="#">Моя группа</a>
+                <a href='#'>Моя группа</a>
               </li>
               <li>
-                <a href="#">Настройки</a>
+                <a href='#'>Настройки</a>
               </li>
               <li>
-                <a href="#">Выход</a>
+                <a href='#'>Выход</a>
               </li>
             </ul>
           </div>
-
-          <ul className="nav__list">
+          <ul className='nav__list'>
             <li>
-              <a className="nav-link" href="#">
+              <a className='nav-link' href='#'>
                 Главная
               </a>
             </li>
             <li>
-              <a className="nav-link" href="#">
+              <a className='nav-link' href='#'>
                 Контакты
               </a>
             </li>
             <li>
-              <a className="nav-link" href="#">
+              <a className='nav-link' href='#'>
                 Каталог
               </a>
             </li>
             <li>
-              <a className="nav-link" href="#">
+              <a className='nav-link' href='#'>
                 Учителя
               </a>
             </li>
