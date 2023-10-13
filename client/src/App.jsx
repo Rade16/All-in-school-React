8a716acd41142e3fa2b62d3c6549594f85bc0dd1
Roman <145358556+Rade16@users.noreply.github.com>
2023-10-14 01:06:55 +0300

    Added new modal window and bug fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,15 @@ import Header from './pages/Header/Header';
 import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
 
-import Nav2 from './components/Nav2/Nav2';
+// import Nav2 from './components/Nav2/Nav2';
 import Profile from './pages/Profile/Profile';
 import Courses from './pages/Courses/Courses';
-// import Nav from './components/Nav/Nav';
+import Nav from './components/Nav/Nav';
 function App() {
   return (
     <>
-      <Nav2 />
-      {/* <Nav /> */}
+      {/* <Nav2 /> */}
+      <Nav />
 
       {/* <Auth/> */}
       <Header />
--- client/src/components/Nav2/Nav2.jsx
+++ client/src/components/Nav2/Nav2.jsx
@@ -1,63 +1,78 @@
-import './_nav2.scss';
-import logo from './../../img/logo.svg';
-
+import "./_nav2.scss";
+import logo from "./../../img/logo.svg";
+import Modal from "./../Modal/Modal";
+import { useState } from "react";
 const Nav2 = () => {
+  const [modalActive, setModalActive] = useState(true);
   return (
-    <header className='header__nav'>
-      <div className='header__container__nav'>
-        <nav className='nav'>
-          <div className='nav__menu'>
-            <input type='checkbox' className='nav__checkbox' />
-            <p className='nav-link nav-link_menu'>Меню</p>
+    <header className="header__nav">
+      <div className="header__container__nav">
+        <nav className="nav">
+          <div className="nav__menu">
+            <input type="checkbox" className="nav__checkbox" />
+            <p className="nav-link nav-link_menu">Меню</p>
 
-            <ul className='menu'>
+            <ul className="menu">
               <li>
-                <a href='#'>Профиль</a>
+                <a href="#">Профиль</a>
               </li>
               <li>
-                <a href='#'>Мои курсы</a>
+                <a href="#">Мои курсы</a>
               </li>
               <li>
-                <a href='#'>Пройденный материал</a>
+                <a href="#">Пройденный материал</a>
               </li>
               <li>
-                <a href='#'>Статистика</a>
+                <a href="#">Статистика</a>
               </li>
               <li>
-                <a href='#'>Моя группа</a>
+                <a href="#">Моя группа</a>
               </li>
               <li>
-                <a href='#'>Настройки</a>
+                <a href="#">Настройки</a>
               </li>
               <li>
-                <a href='#'>Выход</a>
+                <a onClick={() => setModalActive(true)}>Выход</a>
               </li>
             </ul>
           </div>
-          <ul className='nav__list'>
+          <ul className="nav__list">
             <li>
-              <a className='nav-link' href='#'>
+              <a className="nav-link" href="#">
                 Главная
               </a>
             </li>
             <li>
-              <a className='nav-link' href='#'>
+              <a className="nav-link" href="#">
                 Контакты
               </a>
             </li>
             <li>
-              <a className='nav-link' href='#'>
+              <a className="nav-link" href="#">
                 Каталог
               </a>
             </li>
             <li>
-              <a className='nav-link' href='#'>
+              <a className="nav-link" href="#">
                 Учителя
               </a>
             </li>
           </ul>
         </nav>
       </div>
+      <Modal active={modalActive} setActive={setModalActive}>
+        <div className="quit">
+          <div className="quit__container">
+            <div className="quit__text">
+              <p>Вы действитель хотите выйти из аккаунта?</p>
+            </div>
+            <div className="quit__buttons-line">
+              <button className="quit__button">Да</button>
+              <button className="quit__button">Нет</button>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </header>
   );
 };
