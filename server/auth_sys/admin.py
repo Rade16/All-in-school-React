d9bd3f46ff69dde3a6952573ea0b379ad6105d85
Oleg Mishutin <omisutin646@gmail.com>
2023-10-15 22:02:01 +0300

    fixed, added features in admin panel

--- server/auth_sys/admin.py
+++ server/auth_sys/admin.py
@@ -1,16 +1,23 @@
+import os
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import Profile
 
 
 class AdminProfile(admin.ModelAdmin):
-    list_display = ['user', 'first_name', 'second_name', 'last_name', 'telephone']
+    list_display = ['user', 'first_name', 'second_name', 'last_name', 'telephone', 'gender']
     fieldsets = [
         ('Отношение к пользователю', {'fields': ['user']}),
         ('Основная информация', {'fields': ['first_name', 'second_name', 'last_name', 'gender']}),
         ('Второстепенная информация', {'fields': ['telephone', 'photo']})
     ]
     search_fields = ['user', 'telephone']
+    list_filter = [('gender', admin.ChoicesFieldListFilter)]
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            os.remove(obj.photo.path)
+            obj.delete()
 
 
 admin.site.register(Profile, AdminProfile)
--- server/courses/admin.py
+++ server/courses/admin.py
@@ -1,4 +1,20 @@
+import os
 from django.contrib import admin
 from .models import Course
 
-admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    fieldsets = [
+        ('Информация о курсе', {'fields': ['name', 'price', 'image']})
+    ]
+    search_fields = ['name']
+    ordering = ['-price']
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            os.remove(obj.image.path)
+            obj.delete()
+
+
+admin.site.register(Course, CourseAdmin)
