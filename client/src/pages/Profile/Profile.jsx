45e475a3cb8d7f54918f8ba6add5b267109bbee6
Roman <145358556+Rade16@users.noreply.github.com>
2023-10-20 15:49:41 +0300

    Fixed

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -20,23 +20,27 @@ function Profile(props) {
             <form action=''>
               <div className='profile__form-input'>
                 <p className='profile__form-input-text'>Фамилия*</p>
-                <input placeholder='' type='text' className='profile__form-input-field' />
+                <input placeholder='Иванов' type='text' className='profile__form-input-field' />
               </div>
               <div className='profile__form-input'>
                 <p className='profile__form-input-text'>Имя*</p>
-                <input placeholder='' type='text' className='profile__form-input-field' />
+                <input placeholder='Иван' type='text' className='profile__form-input-field' />
               </div>
               <div className='profile__form-input'>
                 <p className='profile__form-input-text'>Отчество*</p>
-                <input placeholder='' type='text' className='profile__form-input-field' />
+                <input placeholder='Иванович' type='text' className='profile__form-input-field' />
               </div>
               <div className='profile__form-input'>
                 <p className='profile__form-input-text'>Номер*</p>
-                <input placeholder='' type='number' className='profile__form-input-field' />
+                <input placeholder='+7826754395' type='number' className='profile__form-input-field' />
               </div>
               <div className='profile__form-input'>
                 <p className='profile__form-input-text'>E-mail*</p>
-                <input placeholder='' type='email' className='profile__form-input-field' />
+                <input placeholder='pochta@gmail.com' type='email' className='profile__form-input-field' />
+              </div>
+              <div className='profile__form-input'>
+                <p className='profile__form-input-text'>Telegram*</p>
+                <input placeholder='https://t.me/Cheburashka' type='text' className='profile__form-input-field'  />
               </div>
               <button className='profile__form-button'>Редактировать данные</button>
             </form>
