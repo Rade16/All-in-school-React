7ff601d6c11b258835bed13acd55455d1490d3fb
Roman <145358556+Rade16@users.noreply.github.com>
2023-10-21 19:07:59 +0300

    Fixed

--- client/src/components/CourseVideo/CourseVideo.jsx
+++ client/src/components/CourseVideo/CourseVideo.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import "./_courseVideo.scss";
 import ShakaPlayer from "shaka-player-react";
 import "shaka-player/dist/controls.css";
-
+import video from "./../../video/GONE.Fludd — TRAXXXMANIA.mp4"
 const CourseVideo = () => {
+  
   return (
     <div className="courseVideo">
       <div className="courseVideo__container">
         <h1 className="courseVideo__title">Парикмахер-стилист</h1>
         <h1 className="courseVideo__lesson-number">Урок 1</h1>
         <div className="courseVideo__video">
-          <ShakaPlayer autoPlay src="./../../img/1.mp4" />
+          <ShakaPlayer autoPlay src={video} />
         </div>
         <div className="courseVideo__description-block">
           <h2 className="courseVideo__description-title">
@@ -22,6 +23,9 @@ const CourseVideo = () => {
             филировка и как они могут влиять на состояние и внешний вид
             Подробнее...
           </p>
+        </div>
+        <div className="courseVideo__button">
+          Следующий урок
         </div>
       </div>
     </div>
